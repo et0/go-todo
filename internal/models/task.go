4c0f00518adea67c19af69aad213291a34226517
Lenya Nab <nabiev.lenya@gmail.com>
2025-03-30 19:51:00 +0400

    Change format for CreatedAt

--- internal/models/task.go
+++ internal/models/task.go
@@ -4,5 +4,5 @@ type Task struct {
 	Id        int    `json:"id"`         // Идентификатор задачи
 	Title     string `json:"title"`      // Описание задачи
 	Complated int    `json:"complated"`  // Статус выполнения 0 или 1
-	CreatedAt int    `json:"created_at"` // Время добавления задачи
+	CreatedAt string `json:"created_at"` // Время добавления задачи
 }
