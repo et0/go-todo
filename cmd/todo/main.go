b22f207081baec838d444ab24a4f6b1c0cf2e28e
Lenya Nab <nabiev.lenya@gmail.com>
2025-03-30 19:21:14 +0400

    Add function with create new task

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -3,21 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/et0/go-todo/internal/models"
 	"github.com/et0/go-todo/internal/storage"
 )
 
 func main() {
-	fmt.Println(os.Args)
 	if len(os.Args) < 2 {
-		fmt.Println("Не хватает аргументов")
+		fmt.Println(models.WrongArgs)
 		os.Exit(0)
 	}
 
 	var tasks = []models.Task{}
 	storage.Load(&tasks)
 	defer storage.Save(&tasks)
+
+	switch os.Args[1] {
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println(models.WrongArgsAdd)
+			os.Exit(0)
+		}
+		now := time.Now()
+		tasks = append(tasks, models.Task{Id: len(tasks) + 1, Title: os.Args[2], Complated: 0, CreatedAt: int(now.Unix())})
+	case "list":
+	case "done":
+	case "delete":
+	case "clear":
+	}
 
 	fmt.Println(tasks)
 }
