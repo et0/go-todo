e29dfc150dbe8456ea03764f41bce9674390526b
Lenya Nab <nabiev.lenya@gmail.com>
2025-03-30 20:10:01 +0400

    Add option list

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -25,13 +25,29 @@ func main() {
 			fmt.Println(models.WrongArgsAdd)
 			os.Exit(0)
 		}
-		now := time.Now()
-		tasks = append(tasks, models.Task{Id: len(tasks) + 1, Title: os.Args[2], Complated: 0, CreatedAt: int(now.Unix())})
+
+		now := time.Now().Local()
+		tasks = append(tasks, models.Task{
+			Id:        len(tasks) + 1,
+			Title:     os.Args[2],
+			Completed: 0,
+			CreatedAt: now.Format("02.01.2006"),
+		})
 	case "list":
+		isCompleted := false
+		isCheck := " "
+		if len(os.Args) == 3 && os.Args[2] == "--completed" {
+			isCompleted = true
+			isCheck = "\u2713"
+		}
+		for _, t := range tasks {
+			if (isCompleted && t.Completed == 0) || (!isCompleted && t.Completed == 1) {
+				continue
+			}
+			fmt.Printf("%d. [%s] %s (%s)\n", t.Id, isCheck, t.Title, t.CreatedAt)
+		}
 	case "done":
 	case "delete":
 	case "clear":
 	}
-
-	fmt.Println(tasks)
 }
